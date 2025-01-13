ffebdd99b7d719dd81f17eab31c3563a7b6a67ec
fynnxxn <fynnian.kolbe@gmail.com>
2025-01-13 21:16:45 +0100

    update

--- Recrutify-Desktop/src/Main.java
+++ Recrutify-Desktop/src/Main.java
@@ -1,9 +1,29 @@
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class Main {
+public class Main extends Application {
+
+    User user = null;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginButton;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        launch(args);
+        /** Scanner scanner = new Scanner(System.in);
         User user = null;
         int decision = 0;
 
@@ -42,6 +62,35 @@ public class Main {
                 Boolean admin = scanner.nextBoolean();
                 UserService.register(username, password, company, firstName, lastName, admin);
                 break;
+        } **/
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Lädt die FXML-Datei
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+
+        // Erstellt eine Scene mit dem geladenen FXML-Inhalt
+        Scene scene = new Scene(root);
+
+        // Setzt die Stage auf "undecorated", um die Titelleiste zu entfernen
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        // Setzt die Scene auf die Bühne (Stage)
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Recrutify | Login");
+        primaryStage.show();
+    }
+
+    @FXML
+    private void LoginButtonAction() {
+        String enteredUsername = username.getText();
+        String enteredPassword = password.getText();
+
+        try {
+            user = UserService.login(enteredUsername, enteredPassword);
+            System.out.println("Eingeloggter User: \n" + user.toString());
+        } catch (Exception e) {
+            System.out.println("Benutzername oder Passwort falsch. Bitte erneut versuchen.");
         }
     }
 }
