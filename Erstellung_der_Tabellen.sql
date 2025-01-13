bd84b55869bb94db24084b44612c0c31a9d7d266
fynnxxn <fynnian.kolbe@gmail.com>
2025-01-13 23:16:05 +0100

    create

---
+++ Erstellung_der_Tabellen.sql
@@ -0,0 +1,52 @@
+-- Tabelle Unternehmen
+CREATE TABLE Unternehmen (
+                             UID INTEGER PRIMARY KEY AUTOINCREMENT,
+                             Name VARCHAR(255) NOT NULL,
+                             Benutzername VARCHAR(255) NOT NULL,
+                             Passwort VARCHAR(255) NOT NULL,
+                             Vorname VARCHAR(255),
+                             Nachname VARCHAR(255),
+                             is_admin BOOLEAN NOT NULL DEFAULT 0
+);
+
+-- Tabelle Test
+CREATE TABLE Test (
+                      TID INT PRIMARY KEY,
+                      Dauer TIME,
+                      UID INT,
+                      FOREIGN KEY (UID) REFERENCES Unternehmen(UID)
+);
+
+-- Tabelle Bewerber
+CREATE TABLE Bewerber (
+                          BID INT PRIMARY KEY,
+                          Vorname VARCHAR(255),
+                          Nachname VARCHAR(255),
+                          Ergebnis INT
+);
+
+-- Tabelle Multiple-Choice-Fragen
+CREATE TABLE MultipleChoiceFragen (
+                                      FID INT PRIMARY KEY,
+                                      Text TEXT,
+                                      Antwort_1 TEXT,
+                                      Antwort_2 TEXT,
+                                      Antwort_3 TEXT,
+                                      Antwort_4 TEXT,
+                                      Richtig_1 BOOLEAN,
+                                      Richtig_2 BOOLEAN,
+                                      Richtig_3 BOOLEAN,
+                                      Richtig_4 BOOLEAN,
+                                      AID INT,
+                                      TID INT,
+                                      FOREIGN KEY (TID) REFERENCES Test(TID)
+);
+
+-- Tabelle Bewerber_Test (Beziehung zwischen Bewerber und Test)
+CREATE TABLE Bewerber_Test (
+                               BID INT,
+                               TID INT,
+                               PRIMARY KEY (BID, TID),
+                               FOREIGN KEY (BID) REFERENCES Bewerber(BID),
+                               FOREIGN KEY (TID) REFERENCES Test(TID)
+);
