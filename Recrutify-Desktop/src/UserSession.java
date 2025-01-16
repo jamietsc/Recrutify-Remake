14a79d90ae866f6f10ef4124217f96a940cc7e56
fynnxxn <fynnian.kolbe@gmail.com>
2025-01-16 15:20:09 +0100

    create

---
+++ Recrutify-Desktop/src/UserSession.java
@@ -0,0 +1,11 @@
+public class UserSession {
+    private static User currentUser;
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+}
