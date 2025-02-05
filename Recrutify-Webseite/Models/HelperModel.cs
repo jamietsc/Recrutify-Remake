5ff632d2dbc572d69ae29a20dd4aa67cad89de3c
FinnLuca05 <finnluca.ritschel@icloud.com>
2025-02-06 00:52:41 +0100

    Bugfixing Erste FID und Timer

--- Recrutify-Webseite/Models/HelperModel.cs
+++ Recrutify-Webseite/Models/HelperModel.cs
@@ -4,6 +4,8 @@
     public class HelperModel
     {
         public int currentFID { get; set; } = 1;
+        public int firstFID { get; set; } = 1;
+        public int currentQuestionNumber => currentFID - firstFID + 1;
         public int currentFreeTextAnswer { get; set; } = 0;
         public bool useTimer { get; set; } = true;
     }
